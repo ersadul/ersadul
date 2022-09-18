6192ecb6de79842d5fb747655fe59c7edbc9b786
Ersa Sa'dul <ersasadulashar@gmail.com>
2022-09-19 01:34:32 +0700

    commit a part of course intro db in py course by niomic

--- python/niomic/Introduction To Databases In Python/main.py
+++ python/niomic/Introduction To Databases In Python/main.py
@@ -89,12 +89,12 @@
 
 
 # # Ordering ( 5 )
-import sqlalchemy as db
+# import sqlalchemy as db
 
-engine = db.create_engine('sqlite:///census.sqlite')
-conn = engine.connect()
-metadata = db.MetaData()
-census = db.Table('census', metadata, autoload=True, autoload_with=engine)
+# engine = db.create_engine('sqlite:///census.sqlite')
+# conn = engine.connect()
+# metadata = db.MetaData()
+# census = db.Table('census', metadata, autoload=True, autoload_with=engine)
 
 # Ordering
 # query = db.select([census.columns.state])
@@ -107,7 +107,112 @@ census = db.Table('census', metadata, autoload=True, autoload_with=engine)
 # print(conn.execute(query).fetchall()[:10])
 
 # Order by Descending
-query = db.select([census.columns.state]).order_by(
-    db.desc(census.columns.state)
-)
-print(conn.execute(query).fetchall()[:10])
+# query = db.select([census.columns.state]).order_by(
+#     db.desc(census.columns.state)
+# )
+# print(conn.execute(query).fetchall()[:10])
+
+# Order by Multiple
+# query = db.select([census.columns.state
+#                   , census.columns.sex]).order_by(
+#                     census.columns.state
+#                     , census.columns.sex
+#                   )
+# print(conn.execute(query).first())
+
+
+# # Counting, Summing and Grouping Data ( 6 )
+# import sqlalchemy as db
+# engine = db.create_engine('sqlite:///census.sqlite')
+# conn = engine.connect()
+# metadata = db.MetaData()
+# census = db.Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Sum Example
+# query = db.select([db.func.sum(census.columns.pop2008)])
+# print(conn.execute(query).scalar())
+
+# SUM - Group by
+# query = db.select([census.columns.sex
+#                   , db.func.sum(census.columns.pop2008)]
+#                   ).group_by(
+#                     census.columns.sex
+#                   )
+# print(conn.execute(query).fetchall())
+
+# SUM - Multiple Group by
+# query = db.select([census.columns.sex, census.columns.age
+#                   , db.func.sum(census.columns.pop2008)]).group_by(
+#                     census.columns.sex, census.columns.age
+#                   )
+# print(conn.execute(query).fetchall())
+
+# # Use Pandas and Matplotlib to Visualize Data ( 7 )
+# # #%% # uncomment this when you wanna show the plot 
+# import sqlalchemy as db
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# engine = db.create_engine('sqlite:///census.sqlite')
+# conn = engine.connect()
+# metadata = db.MetaData()
+# census = db.Table('census', metadata, autoload=True, autoload_with=engine)
+
+# # DataFrame Example
+
+# query = db.select([census.columns.sex, (census.columns.pop2008).label('sum_pop2008')]) \
+#     .order_by(census.columns.sex)
+# result = conn.execute(query).fetchall()
+# # print(result)
+# df = pd.DataFrame(result)
+# df.columns = result[0].keys()
+# # print(df)
+
+# # Graphing
+# df[10:20].plot.barh()
+# plt.show()
+
+# # Calculating Values in a Query ( 8 )
+import sqlalchemy as db
+from sqlalchemy import case
+
+engine = db.create_engine('sqlite:///census.sqlite')
+conn = engine.connect()
+metadata = db.MetaData()
+census = db.Table('census', metadata, autoload=True, autoload_with=engine)
+
+# # Calculating Difference
+# query = db.select([census.columns.age, (census.columns.pop2008 
+#     - census.columns.pop2000).label('pop_change')]
+#     ).order_by(db.desc('pop_change')).limit(5)
+
+# print(conn.execute(query).fetchall())
+
+# Case Statement
+# query = db.select([
+#     db.func.sum(
+#         case([
+#             (census.columns.state == 'New York'
+#             , census.columns.pop2008)
+#         ], else_=0)
+#     )
+# ])
+
+# print(conn.execute(query).fetchall())
+
+# Percentage Example
+# query = db.select([
+#     (
+#         db.func.sum(
+#             case([
+#                 (census.columns.state == 'New York'
+#                  , census.columns.pop2008)
+#             ], else_=0) 
+#         ) / db.cast(db.func.sum(census.columns.pop2008)
+#                  , db.Float) * 100
+#     ).label('ny_percent')
+# ])
+
+# print(conn.execute(query).fetchall())
+
+# # SQL Relationships ( 9 )
