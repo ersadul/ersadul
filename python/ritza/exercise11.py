17282e81bd561f7cdc2f1f356bd0357dffc0c36a
Ersa Sa'dul <ersasadulashar@gmail.com>
2022-09-22 02:23:01 +0700

    commit chapter12 and exercise11

--- python/ritza/exercise11.py
+++ python/ritza/exercise11.py
@@ -2,6 +2,61 @@
 import sys
 
 
+def add_vectors(u, v):
+    vector = []
+    if len(u) == len(v):
+        for i in range(len(u)):
+            vector.append(u[i] + v[i])
+    return vector
+
+
+def scalar_mult(s, v):
+    tmp = []
+    for i in range(len(v)):
+        new_list.append(s * v[i])
+    return tmp
+
+
+def dot_product(u, v):
+    tmp = 0
+    if len(u) == len(v):
+        for i in range(len(u)):
+            tmp += u[i] * v[i]
+    return tmp
+
+[
+    [1, 2, 3],
+    [2, 3, 1],
+    [5, 1, 2]
+]
+
+def cross_product(u, v):
+    tmp = []
+    if len(u) == len(v):
+        tmp.append(u[1] * v[2] - u[2] * v[1])
+        tmp.append(u[2] * v[0] - u[0] * v[2])
+        tmp.append(u[0] * v[1] - u[1] * v[0])
+    return tmp
+
+
+def replace(s, old, new):
+    if len(old) == 1:
+        words = [ch for ch in s]
+        for i, word in enumerate(words):
+            if word == old:
+                words[i] = new
+
+        return ''.join(words)
+
+    else:
+        words = s.split()
+        for i, word in enumerate(words):
+            if old in word:
+                words[i] = word[:word.index(old)] + new + word[word.index(old)+len(old):]
+                
+        return ' '.join(words)
+
+
 def test(did_pass):
     """ Print the result of a test. """
     linenum = sys._getframe(1).f_lineno
@@ -14,6 +69,34 @@ def test(did_pass):
 
 def test_suite():
     """ Run the suite of test for code in this module. """
+    # # 5. 
+    # test(add_vectors([1, 1], [1, 1]) == [2, 2])
+    # test(add_vectors([1, 2], [1, 4]) == [2, 6])
+    # test(add_vectors([1, 2, 1], [1, 4, 3]) == [2, 6, 4])    
+
+    # # 6. 
+    # test(scalar_mult(5, [1, 2]) == [5, 10])
+    # test(scalar_mult(3, [1, 0, -1]) == [3, 0, -3])
+    # test(scalar_mult(7, [3, 0, 5, 11, 2]) == [21, 0, 35, 77, 14])
+    
+    # # 7. 
+    # test(dot_product([1, 1], [1, 1]) == 2)
+    # test(dot_product([1, 2], [1, 4]) == 9)
+    # test(dot_product([1, 2, 1], [1, 4, 3]) == 12)
+    
+    # # 8.
+    # test(cross_product([1, 2, 3], [4, 5, 6]) == [-3, 6, -3])
+    # test(cross_product([2, 1, 3], [1, 2, 5]) == [-1, -7, 3])
+
+    # # 10.
+    # test(replace("Mississippi", "i", "I") == "MIssIssIppI")
+    # s = "I love spom! Spom is my favorite food. Spom, spom, yum!"
+    # test(replace(s, "om", "am") ==
+    # "I love spam! Spam is my favorite food. Spam, spam, yum!")
+    # test(replace(s, "o", "a") ==
+    # "I lave spam! Spam is my favarite faad. Spam, spam, yum!")
+
+test_suite()
 
 
 # 1. What is the Python interpreter’s response to the following?
@@ -76,3 +159,60 @@ def test_suite():
 # print("Test 1: {0}".format(this is that)) # output: Test 1: False
 # that = this
 # print("Test 2: {0}".format(this is that)) # output: Test 2: True
+
+# 5. Lists can be used to represent mathematical vectors. In this exercise and several that follow you
+# will
+# write functions to perform standard operations on vectors. Create a script named vectors.py and
+# write Python code to pass the tests in each case.
+# Write a function add_vectors(u, v) that takes two lists of numbers of the same length, and returns
+# a new list containing the sums of the corresponding elements of each:
+# test(add_vectors([1, 1], [1, 1]) == [2, 2])
+# test(add_vectors([1, 2], [1, 4]) == [2, 6])
+# test(add_vectors([1, 2, 1], [1, 4, 3]) == [2, 6, 4])
+
+# 6. Write a function scalar_mult(s, v) that takes a number, s, and a list, v and returns the scalar
+# multiple¹⁰ of v by s:
+# test(scalar_mult(5, [1, 2]) == [5, 10])
+# test(scalar_mult(3, [1, 0, -1]) == [3, 0, -3])
+# test(scalar_mult(7, [3, 0, 5, 11, 2]) == [21, 0, 35, 77, 14])
+
+# 7. Write a function dot_product(u, v) that takes two lists of numbers of the same length, and returns
+# the
+# sum of the products of the corresponding elements of each (the dot_product¹¹).
+# test(dot_product([1, 1], [1, 1]) == 2)
+# test(dot_product([1, 2], [1, 4]) == 9)
+# test(dot_product([1, 2, 1], [1, 4, 3]) == 12)
+
+# 8. Extra challenge for the mathematically inclined: Write a function cross_product(u, v) that
+# takes two lists of numbers of length 3 and returns their cross product¹². You should write your own tests.
+
+# 9. Describe the relationship between " ".join(song.split()) and song in the fragment of code
+# below. Are they the same for all strings assigned to song? When would they be different?
+# Answer: the are the same. it will be different when the character that used not ' '
+# song = "The rain in Spain..."
+# print(" ".join(song.split()))
+# print((song.split()))
+
+# 10. Write a function replace(s, old, new) that replaces all occurrences of old with new in a strings:
+# test(replace("Mississippi", "i", "I") == "MIssIssIppI")
+# s = "I love spom! Spom is my favorite food. Spom, spom, yum!"
+# test(replace(s, "om", "am") ==
+# "I love spam! Spam is my favorite food. Spam, spam, yum!")
+# test(replace(s, "o", "a") ==
+# "I lave spam! Spam is my favarite faad. Spam, spam, yum!")
+# Hint: use the split and join methods.
+
+# 11. Suppose you want to swap around the values in two variables. You decide to factor this out into a
+# reusable function, and write this code:
+# Answer: this function will not swap the value because value inside the function is not returned
+# def swap(x, y): # Incorrect version
+#     print("before swap statement: x:", x, "y:", y)
+#     (x, y) = (y, x)
+#     print("after swap statement: x:", x, "y:", y)
+
+# a = ["This", "is", "fun"]
+# b = [2,3,4]
+# print("before swap function call: a:", a, "b:", b)
+# swap(a, b)
+# print("after swap function call: a:", a, "b:", b)
+
